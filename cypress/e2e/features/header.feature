029f4f408effeba4e0d0e51b98c46f5fa8b63582
Samanta1984 <mybootcampqa@gmail.com>
2024-11-05 20:28:34 +0100

    added test linkedin

--- cypress/e2e/features/header.feature
+++ cypress/e2e/features/header.feature
@@ -2,6 +2,21 @@ Feature: Visit Portfolio header section
 
     As recruiter, I want to see a header with the candidate name, title and links to Linkedin and Github, so I can know if fits with the position
 
+Scenario: See header information
+    Given the user visit portfolio webpage
+    Then the user see name "Reyes Diaz" on header
+    And the user see title contain "QA" on header
+    And the user see github link "https://github.com/Bootcamp-QA/portfolioqa"
+    And the user see linkedin link "https://www.linkedin.com/in/reyescuesta"
+
 Scenario: Visit github link
     Given the user visit portfolio webpage
     When the user click on github link
+
+Scenario: Visit linkedin link
+    Given the user visit portfolio webpage
+    When the user click on linkedin link
+
+
+
+
--- cypress/e2e/step_definitions/header_steps.js
+++ cypress/e2e/step_definitions/header_steps.js
@@ -15,4 +15,43 @@ import {
     //Hacer click derecho
     //cy.get('[data-testid="githublink"]').rightclick();
 
-  })
+  });
+
+  When ("the user click on linkedin link", () => {
+
+    //Obtenemos el elemento del enlace y hacemos click
+    cy.get('[data-testid="linkedinlink"]').click();
+
+  });
+
+  Then ("the user see name {string} on header", (name) => {
+    //obtener el elemento del nombre y comprueba que tiene el TEXTO EXACTO
+    cy.get('[data-testid="headername"]').should('have.text',name);
+
+
+  });
+
+
+
+  Then ("the user see title contain {string} on header", (title) => {
+   //obtener el elemento del titulo y comprueba que CONTIENE el texto
+   cy.get('[data-testid="headertitle"]').should('contain',title);
+
+
+  });
+
+
+Then ("the user see github link {string}", (githublink) => {
+ //obtenemos el elemento con el enlace a github y comprobamos que tiene la url de github esperada
+  cy.get('[data-testid="githublink"').should('have.attr', 'href',githublink);
+
+  });
+
+  Then ("the user see linkedin link {string}", (linkedinlink) => {
+    //obtenemos el elemento con el enlace a linkedin y comprobamos que tiene la url de linkedin esperada
+    cy.get('[data-testid="linkedinlink"').should('have.attr', 'href',linkedinlink);
+  
+    });
+
+
+
